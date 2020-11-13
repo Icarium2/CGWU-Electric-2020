d72fb15fbf73e68b9956d82f6c2b84ff663a5bf7
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-11-13 15:31:13 +0100

    Added menu
    Added a a responsive menu. Still some kinks to sort out with coloring, positioning and contrasting menu-items with the image. Will add a close-button later for when the burger is toggled.

---
+++ app.js
@@ -0,0 +1,23 @@
+const navAnimation = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-items");
+  const navItems = document.querySelectorAll(".nav-items li");
+
+  //toggle nav
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navItems.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navItemFade 0.5s ease forwards ${
+          index / 8 + 0.3
+        }s`;
+      }
+    });
+  });
+};
+
+navAnimation();
