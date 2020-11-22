c3075048be4f9b956249b250d17897d1975036c9
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-11-22 14:05:46 +0100

    .
    .

--- order.js
+++ order.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const colour_btn = document.querySelectorAll(".colours .colour");
-  const exterior = document.querySelector(".imagery .exterior");
-  const interior = document.querySelector(".imagery .interior");
+  const exterior = document.querySelector(".images .exterior");
+  const interior = document.querySelector(".images .interior");
 
   //click-function that changes the colours of buttons - and also changes images based on which button is clicked //
   for (let i = 0; i < colour_btn.length; i++) {
@@ -15,6 +15,8 @@ window.onload = function () {
       exterior.src = "images/customize/electric-" + this.dataset.name + ".png";
     });
   }
+
+  //The same function, except it gets the interior-images instead of the exterior ones //
   for (let i = 0; i < colour_btn.length; i++) {
     let btn = colour_btn[i];
 
