0f19a380f4765f365fce465f105ad345a574f405
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-11-23 17:44:05 +0100

    Update order.js

--- order.js
+++ order.js
@@ -2,9 +2,8 @@ window.onload = function () {
   const colour_btn = document.querySelectorAll(".colours .colour");
   const exterior = document.querySelector(".images .exterior");
   const interior = document.querySelector(".images .interior");
-  const home_btn = document.querySelector(".banner .logo");
 
-  //click-function that changes the colours of buttons - and also changes images based on which button is clicked //
+  //click-function that changes the colours of buttons - and also changes images based on which button is clicked, calling on dataset-names (colours in this case) and the css classes 'colours' and 'selected' //
   for (let i = 0; i < colour_btn.length; i++) {
     let btn = colour_btn[i];
 
@@ -22,7 +21,6 @@ window.onload = function () {
     let btn = colour_btn[i];
 
     btn.addEventListener("click", function () {
-      document;
       interior.src = "images/customize/interior-" + this.dataset.name + ".png";
     });
   }
